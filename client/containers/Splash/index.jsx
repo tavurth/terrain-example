f13fbb64e9f83168fa146dadb9501018dca621fd
Will <Tavurth@gmail.com>
2017-11-12 01:10:07 +0300

    prettier

--- client/containers/Splash/index.jsx
+++ client/containers/Splash/index.jsx
@@ -1,24 +1,23 @@
-"use strict"
+'use strict';
 
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 import FlatButton from 'material-ui/FlatButton';
 
-import Config from 'modules/Config'
-import Game from 'containers/Game'
+import Config from 'modules/Config';
+import Game from 'containers/Game';
 
-import './index.scss'
+import './index.scss';
 
 class Splash extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     selectQuality(setting) {
         this.props.dispatch({
             splash: false,
-            type: 'loader'
+            type: 'loader',
         });
 
         Game.start('main-render-canvas', setting);
@@ -31,30 +30,30 @@ class Splash extends React.Component {
     }
 
     render() {
-        let qualitySettings = [
-            'ultra-low', 'low', 'medium', 'high', 'very-high'
-        ];
+        let qualitySettings = ['ultra-low', 'low', 'medium', 'high', 'very-high'];
 
         let hasSplash = (
-            <div className='full-size splash-screen'>
+            <div className="full-size splash-screen">
                 <div>
                     <h2>Quality level</h2>
-                    <div id='quality-levels'>
-                        {
-                            qualitySettings.map((setting, id) => {
-                                return (<FlatButton
-                                            default={true}
-                                            key={setting + '-quality-setting'}
-                                            onTouchTap={() => this.selectQuality(setting)}
-                                        >{setting}</FlatButton>)
-                            })
-                        }
+                    <div id="quality-levels">
+                        {qualitySettings.map((setting, id) => {
+                            return (
+                                <FlatButton
+                                    default={true}
+                                    key={setting + '-quality-setting'}
+                                    onTouchTap={() => this.selectQuality(setting)}
+                                >
+                                    {setting}
+                                </FlatButton>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
         );
 
-        return this.props.splash ? hasSplash : <div className='full-size'/>;
+        return this.props.splash ? hasSplash : <div className="full-size" />;
     }
 }
 
@@ -62,7 +61,7 @@ class Splash extends React.Component {
 let mapStateToProps = state => {
     return {
         splash: state.loader.splash,
-    }
+    };
 };
 
 export default connect(mapStateToProps)(Splash);
